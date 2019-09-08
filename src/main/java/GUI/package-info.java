52ae566b27b3241781b102f855cfb05c561ca226
Arda Ntourali <ardadrl@outlook.com>
2019-09-08 17:21:47 +0200

    Initial commit.

---
+++ src/main/java/GUI/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * GUI classes that includes the foundational elements for a Backgammon game
+ * <p>
+ * These classes do not include any logic and requires a logic package.
+ *
+ * @since 1.0a
+ * @author rdadrl
+ * @version 1.0a
+ */
+package GUI;
