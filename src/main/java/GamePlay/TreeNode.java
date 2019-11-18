540fd53db14e01b8148a4a99830dc5622cbfb289
Laurenz Ohnemüller <lohnemueller@hotmail.com>
2019-11-18 17:11:13 +0100

    Tree Node

--- src/main/java/GamePlay/TreeNode.java
+++ src/main/java/GamePlay/TreeNode.java
@@ -26,4 +26,39 @@ public class TreeNode {
 
     //If the chip was used to hit an opponent chip- mark.
     boolean hitChip;
+
+    private List<Node> children = new ArrayList<>();
+
+    private Node parent = null;
+
+    //basic method of adding moves
+    public Node addChild(Node child){
+
+        child.setParent(this);
+        this.children.add(child);
+        return child;
+    }
+
+    // some basic method we might need
+    private void setParent(Node parent) {
+
+        this.parent = parent;
+    }
+    public Node<T> getParent() {
+
+        return parent;
+    }
+
+    public int Depth(){
+        int counter = 0;
+
+        Node node = this;
+        while(node.parent != null){
+            counter++;
+            node = node.parent;
+
+        }
+
+        return counter;
+    }
 }
