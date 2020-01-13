8b2ffe803c02af050df08e512239035f904a1140
Arda Ntourali <a.ntourali@student.maastrichtuniversity.nl>
2020-01-13 15:14:01 +0100

    Undo previous merge.

--- src/main/java/PossibleMoveTester.java
+++ src/main/java/PossibleMoveTester.java
@@ -10,10 +10,10 @@ public class PossibleMoveTester {
         Player b = new Player("Libby");
         Board root = new Board(a, b);
         ArrayList<Integer> moves = new ArrayList<>();
-        moves.add(3);
         moves.add(5);
+        moves.add(3);
 
-        ArrayList<int[]>[] possibleMoves = BoardAnalysis.possibleMoves(root, moves, a);
+        ArrayList<int[]>[] possibleMoves = BoardAnalysis.possibleMoves(root, moves, b);
         for (int ma = 0; ma < possibleMoves.length; ma++) {
             System.out.println("-- MOVES FOR DICE " + ma + " --");
             for (int mk = 0; mk < possibleMoves[ma].size(); mk++)
@@ -22,7 +22,7 @@ public class PossibleMoveTester {
 
         ArrayList<int[][]> pC = new ArrayList<int[][]>();
         BoardAnalysis newb = new BoardAnalysis();
-        pC = newb.possibleCombinations(root, moves, a, possibleMoves, possibleMoves[0].size(), possibleMoves[1].size()-1, pC);
+        pC = newb.possibleCombinations(root, moves, a, possibleMoves, possibleMoves[0].size(), possibleMoves[1].size()-1, pC, possibleMoves[1].get(possibleMoves[1].size() - 1), possibleMoves[0].get(possibleMoves[0].size() - 1));
         for (int i = 0; i < pC.size(); i++) {
             System.out.println(pC.get(i)[0][0] + "-"+ pC.get(i)[0][1] +" " + pC.get(i)[1][0] + "-" + pC.get(i)[1][1]);
         }
